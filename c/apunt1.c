0f93b73e9b2235ea6c6b3774c0f2a065e5efe04b
pedroromn <pedroromn@gmail.com>
2017-12-26 23:14:01 -0500

    26/12/2017 23:13

---
+++ c/apunt1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+  int a = 7;   /* a es un entero */
+  int *ptrA;   /* ptrA es un apuntador a un entero  */
+
+  ptrA = &a;
+
+  printf("La dirección de a %p"
+         "\nEl valor de ptrA es %p", &a, ptrA);
+
+  printf("\n");
+  return 0;
+}
